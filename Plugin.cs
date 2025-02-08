c08d391a3367ff7a65eb29bcd7c12443a328eeed
ButteryStancakes <jlj3322@gmail.com>
2025-02-07 19:18:10 -0500

    Migrated shotgun fixes from ButteryFixes

--- Plugin.cs
+++ Plugin.cs
@@ -13,7 +13,7 @@ namespace MeleeFixes
     [BepInPlugin(PLUGIN_GUID, PLUGIN_NAME, PLUGIN_VERSION)]
     public class Plugin : BaseUnityPlugin
     {
-        const string PLUGIN_GUID = "butterystancakes.lethalcompany.meleefixes", PLUGIN_NAME = "Melee Fixes", PLUGIN_VERSION = "1.3.0";
+        const string PLUGIN_GUID = "butterystancakes.lethalcompany.meleefixes", PLUGIN_NAME = "Melee Fixes", PLUGIN_VERSION = "1.4.0";
         internal static new ManualLogSource Logger;
 
         void Awake()
@@ -36,11 +36,13 @@ namespace MeleeFixes
 
         static readonly FieldInfo OBJECTS_HIT_BY_SHOVEL_LIST = AccessTools.Field(typeof(Shovel), "objectsHitByShovelList");
         static readonly FieldInfo OBJECTS_HIT_BY_KNIFE_LIST = AccessTools.Field(typeof(KnifeItem), "objectsHitByKnifeList");
+        static readonly FieldInfo ENEMY_COLLIDERS = AccessTools.Field(typeof(ShotgunItem), "enemyColliders");
 
         static readonly MethodInfo FIND_OBJECT_OF_TYPE_ROUND_MANAGER = AccessTools.Method(typeof(Object), nameof(Object.FindObjectOfType), null, [typeof(RoundManager)]);
         static readonly MethodInfo ROUND_MANAGER_INSTANCE = AccessTools.DeclaredPropertyGetter(typeof(RoundManager), nameof(RoundManager.Instance));
 
-        static readonly MethodInfo MELEE_HELPER_FILTER_TARGETS = AccessTools.Method(typeof(MeleeHelper), nameof(MeleeHelper.FilterTargets));
+        static readonly MethodInfo MELEE_HELPER_FILTER_TARGETS = AccessTools.Method(typeof(WeaponHelper), nameof(WeaponHelper.FilterTargets));
+        static readonly MethodInfo SHOTGUN_PRE_PROCESS = AccessTools.Method(typeof(WeaponHelper), nameof(WeaponHelper.ShotgunPreProcess));
 
         [HarmonyPatch(typeof(Shovel), nameof(Shovel.HitShovel)), HarmonyPriority(Priority.First)]
         [HarmonyPatch(typeof(KnifeItem), nameof(KnifeItem.HitKnife))]
@@ -142,9 +144,61 @@ namespace MeleeFixes
                 Plugin.Logger.LogInfo("Reset broken shovel to allow swinging it again");
             }
         }
+
+        [HarmonyPatch(typeof(ShotgunItem), nameof(ShotgunItem.ShootGun))]
+        [HarmonyTranspiler]
+        static IEnumerable<CodeInstruction> ShotgunItemTransShootGun(IEnumerable<CodeInstruction> instructions, ILGenerator generator)
+        {
+            List<CodeInstruction> codes = instructions.ToList();
+
+            bool fixEarsRinging = false;
+            for (int i = 2; i < codes.Count; i++)
+            {
+                // first distance check for tinnitus/screenshake
+                if (!fixEarsRinging && codes[i].opcode == OpCodes.Bge_Un && codes[i - 2].opcode == OpCodes.Ldloc_2)
+                {
+                    for (int j = i + 1; j < codes.Count - 1; j++)
+                    {
+                        int insertAt = -1;
+                        if (codes[j + 1].opcode == OpCodes.Ldloc_2)
+                        {
+                            // first jump from if/else branches
+                            if (insertAt >= 0 && codes[j].opcode == OpCodes.Br)
+                            {
+                                codes.Insert(insertAt, new(OpCodes.Br, codes[j].operand));
+                                Plugin.Logger.LogDebug("Transpiler (Shotgun blast): Fix ear-ringing severity in extremely close range");
+                                fixEarsRinging = true;
+                                break;
+                            }
+                            // the end of the first if branch
+                            else if (insertAt < 0 && codes[j].opcode == OpCodes.Stloc_S)
+                                insertAt = j + 1;
+                        }
+                    }
+                }
+                else if (codes[i].opcode == OpCodes.Newarr && (System.Type)codes[i].operand == typeof(RaycastHit) && codes[i - 1].opcode == OpCodes.Ldc_I4_S && (sbyte)codes[i - 1].operand == 10)
+                {
+                    codes[i - 1].operand = 50;
+                    Plugin.Logger.LogDebug("Transpiler (Shotgun blast): Resize target colliders array");
+                }
+                else if (codes[i].opcode == OpCodes.Call && codes[i].operand.ToString().Contains("SphereCastNonAlloc"))
+                {
+                    codes.InsertRange(i + 2, [
+                        new(OpCodes.Ldarg_1),
+                        new(OpCodes.Ldloca_S, codes[i + 1].operand),
+                        new(OpCodes.Ldarg_0),
+                        new(OpCodes.Ldflda, ENEMY_COLLIDERS),
+                        new(OpCodes.Call, SHOTGUN_PRE_PROCESS),
+                    ]);
+                    Plugin.Logger.LogDebug("Transpiler (Shotgun blast): Pre-process shotgun targets");
+                }
+            }
+
+            return codes;
+        }
     }
 
-    static class MeleeHelper
+    static class WeaponHelper
     {
         internal static void FilterTargets(ref List<RaycastHit> hits)
         {
@@ -153,6 +207,7 @@ namespace MeleeFixes
             {
                 if (hits[i].transform.TryGetComponent(out IHittable hittable))
                 {
+                    // temporary until vanilla typo is corrected
                     if (!uniqueTargets.Add(hittable))
                     {
                         hits.RemoveAt(i--);
@@ -169,6 +224,61 @@ namespace MeleeFixes
                     }
                 }
             }
+        }
+
+        internal static void ShotgunPreProcess(Vector3 shotgunPosition, ref int num, ref RaycastHit[] results)
+        {
+            int index = 0;
+            HashSet<EnemyAI> enemies = [];
+            List<RaycastHit> invincibles = [];
+
+            // sort in order of distance
+            RaycastHit[] sortedResults = [.. results.Take(num).OrderBy(hit => Vector3.Distance(shotgunPosition, hit.point))];
+
+            // remove all duplicates
+            for (int i = 0; i < num; i++)
+            {
+                if (sortedResults[i].transform.TryGetComponent(out EnemyAICollisionDetect enemyCollider) && !enemyCollider.onlyCollideWhenGrounded)
+                {
+                    EnemyAI enemy = enemyCollider.mainScript;
+                    if (enemies.Add(enemy))
+                    {
+                        EnemyType enemyType = enemy.enemyType;
+                        // invincible enemies are low-priority
+                        if (!enemyType.canDie || enemyType.name == "DocileLocustBees")
+                            invincibles.Add(sortedResults[i]);
+                        else if (!enemy.isEnemyDead)
+                        {
+                            results[index] = sortedResults[i];
+                            index++;
+                            // only hit 10 targets max
+                            if (index == 10)
+                            {
+                                num = 10;
+                                return;
+                            }
+                        }
+                    }
+                }
+            }
+
+            // add invincible enemies at the end, if there are slots leftover
+            if (invincibles.Count > 0)
+            {
+                // slime is "medium priority" since they get angry when shot
+                foreach (RaycastHit invincible in invincibles.OrderByDescending(invincible => invincible.transform.GetComponent<EnemyAICollisionDetect>().mainScript is BlobAI))
+                {
+                    results[index] = invincible;
+                    index++;
+                    if (index == 10)
+                    {
+                        num = 10;
+                        return;
+                    }
+                }
+            }
+
+            num = index;
         }
     }
 }
